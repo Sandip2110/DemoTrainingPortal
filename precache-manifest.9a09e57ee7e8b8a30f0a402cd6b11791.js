8a53119efc523a5cb0b7a30aecc9203692f2335a
sandip2110 <paulsandip2110@gmail.com>
2019-12-02 22:11:33 +0530

    build

---
+++ precache-manifest.9a09e57ee7e8b8a30f0a402cd6b11791.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97676eb683f6b6b34ac1c61aab9bac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2dbbf5b9a379bdb73c1",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b8c81dc625f780e6894f",
+    "url": "/static/css/main.0e39d150.chunk.css"
+  },
+  {
+    "revision": "b2dbbf5b9a379bdb73c1",
+    "url": "/static/js/2.739eeb56.chunk.js"
+  },
+  {
+    "revision": "b8c81dc625f780e6894f",
+    "url": "/static/js/main.074225a8.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "8a5390b32238425448d7c14fe4186790",
+    "url": "/static/media/Kids Reading.8a5390b3.svg"
+  },
+  {
+    "revision": "cadd6c8e3f4c1a771a909591afc79ab3",
+    "url": "/static/media/owl.cadd6c8e.png"
+  }
+]);
